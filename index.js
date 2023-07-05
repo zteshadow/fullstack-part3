4048774afd6e38380bb68c143d1ca6d4482165f4
Samuel Song <samuel.song@agoda.com>
2023-07-05 08:25:20 +0700

    add post

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -21,7 +23,7 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('Hello World')
+    response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
@@ -48,6 +50,17 @@ app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+
+    const note = request.body
+    note.id = maxId + 1
+
+    notes = notes.concat(note)
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
