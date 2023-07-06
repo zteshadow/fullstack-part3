32464894d1a96103c43aed95352caa64c47b98da
Samuel Song <samuel.song@agoda.com>
2023-07-06 08:34:40 +0700

    add persons

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const app = express()
 
 app.use(express.json())
 
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
 let notes = [
     {
       id: 1,
@@ -52,11 +75,29 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
 
-    const note = request.body
-    note.id = maxId + 1
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
 
     notes = notes.concat(note)
     console.log(note)
