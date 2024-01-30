5a1f5e035a742c9aa02762de7bf184ad45ead71d
Samuel Song <samuel.song@agoda.com>
2024-01-30 09:00:35 +0700

    backend connected to MongoDB fine

--- index.js
+++ index.js
@@ -70,9 +70,46 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save().then(savedNote => {
+        console.log(note)
+        response.json(note)
+    })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 // Phonebook
@@ -113,27 +150,6 @@ const generateId = () => {
     const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
     return maxId + 1
 }
-
-// Create
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
-
-    note.save().then(savedNote => {
-        console.log(note)
-        response.json(note)
-    })
-})
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
