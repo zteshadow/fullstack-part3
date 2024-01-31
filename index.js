804786180b49a36c6f5259a0844cf55fb3265694
Samuel Song <samuel.song@agoda.com>
2024-01-31 08:39:25 +0700

    done

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.delete('/api/notes/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) {
@@ -91,10 +91,12 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
+    note.save()
+    .then(savedNote => {
         console.log(note)
         response.json(note)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -156,6 +158,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json( {error: error.message} )
     }
 
     next(error)
