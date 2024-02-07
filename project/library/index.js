0128c8b9275427fa089bd9e673184fc998d64643
Samuel Song <samuel.song@agoda.com>
2024-02-07 09:11:49 +0700

    add excercise

--- project/library/index.js
+++ project/library/index.js
@@ -4,16 +4,87 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 const { GraphQLError } = require('graphql')
 
+let books = [
+    {
+        "title": "Clean Code",
+        "author": "Robert Martin",
+        "published": 2008,
+        "genres": ["refactoring"]
+    },
+    {
+        "title": "Agile software development",
+        "author": "Robert Martin",
+        "published": 2002,
+        "genres": ["agile", "patterns", "design"]
+    },
+    {
+        "title": "Refactoring",
+        "author": "Martin Fowler",
+        "published": 1999,
+        "genres": ["refactoring"]
+    },
+    {
+        "title": "Crime and Punishment",
+        "author": "Fyodor Dostoevsky",
+        "published": 1866,
+        "genres": ["classic", "crime"]
+    }
+]
+
+let authors = [
+    {
+      "name": "Robert Martin",
+      "bookCount": 2
+    },
+    {
+      "name": "Martin Fowler",
+      "bookCount": 1
+    },
+    {
+      "name": "Fyodor Dostoevsky",
+      "bookCount": 2
+    },
+    {
+      "name": "Joshua Kerievsky",
+      "bookCount": 1
+    },
+    {
+      "name": "Sandi Metz",
+      "bookCount": 1
+    },
+  ]
+
 let typeDefs = `
+type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+}
+
+type Author {
+    name: String!
+    bookCount: Int!
+}
+
 type Query {
     bookCount: Int!
     authorCount: Int!
+    allAuthors: [Author!]!
+    allBooks(author: String): [Book!]!
 }
 `
 const resolvers = {
     Query: {
         bookCount: () => 7,
-        authorCount: () => 5
+        authorCount: () => 5,
+        allAuthors: () => authors,
+        allBooks: (root, args) => {
+            if (!args.author) {
+                return books
+            }
+            return books.filter(book => book.author === args.author)
+        }
     }
 }
 
